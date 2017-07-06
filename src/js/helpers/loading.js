29628fbddbf9c7b8f675c6e6fbc72af61398900b
heliistanders <heliistanders@users.noreply.github.com>
2017-07-06 15:39:33 +0200

    added Comments

--- src/js/helpers/loading.js
+++ src/js/helpers/loading.js
@@ -2,23 +2,24 @@
 
 export class Loading{
   constructor(){
+    // parentnode in DOM
     this.content = document.querySelector('#wrapper');
-
+    // create Loading-Spinner
     this.wrapper = document.createElement('div');
     this.wrapper.id = 'loading';
     this.wrapper.style.display = "flex";
     this.wrapper.style.justifyContent = 'center';
     this.wrapper.innerHTML = `<img src="img/load.svg" alt="loading">`;
-
+    // bind context
     this.showLoading = this.showLoading.bind(this);
     this.hideLoading = this.hideLoading.bind(this);
   }
-
+  // display loading-spinner
   showLoading(){
     this.content.innerHTML = '';
     this.content.appendChild(this.wrapper);
   }
-
+  // hide loading spinner
   hideLoading(){
     let loading = document.querySelector('#loading')
     if(loading){
