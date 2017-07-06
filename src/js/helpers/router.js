17bd87236cb6044f71f470e32ac4103657d58c00
heliistanders <heliistanders@users.noreply.github.com>
2017-07-06 15:48:13 +0200

    added Comments

--- src/js/helpers/router.js
+++ src/js/helpers/router.js
@@ -13,11 +13,11 @@ export class Router {
     this.marks = new Marks();
 
     this.isLoadingNewContent = false;
-
+    // bind context to functions
     this.addEventListeners = this.addEventListeners.bind(this);
     this.route = this.route.bind(this);
     this.loadContent = this.loadContent.bind(this);
-
+    // first content to display
     this.welcome_msg = `<div class="welcome_msg">
         <h1 tabindex="0">Welcome!</h1>
         <p tabindex="0">Thank you for using the FH2Go Progressive Web App</p>
@@ -34,18 +34,19 @@ export class Router {
   }
 
   addEventListeners() {
+    // intercept load an popstate changes
     window.addEventListener("load", this.route);
     window.addEventListener("popstate", this.route);
-
+    // intercept every btn/link
     let menu_btns = document.querySelectorAll(".menu_btn");
     Array.from(menu_btns).forEach(btn => {
       btn.addEventListener("click", this.loadContent);
     });
   }
-
+  // changes content depending on the given url
   route() {
     this.message.hideAllMessages();
-
+  
     if (window.location.pathname === "/" || window.location.pathname == "") {
       this.settings.getDefaultPage().then(page => {
         if (page) {
@@ -87,7 +88,7 @@ export class Router {
       this.isLoadingNewContent = false;
     }
   }
-
+  // prevents a reload of page from links -> changes url instead and propagates to route-function
   loadContent() {
     evt.preventDefault();
     let url = new URL(evt.target.href);
